3280d8ae1af171f460c325ab4bcae63012d8a706
Johnny Nguyen <nsaneazylm@gmail.com>
2014-04-23 14:48:30 -0500

    Updated database class and new test main

--- hanabi-cmdline/src/edu/shsu/hanabi_cmdline/database/Database.java
+++ hanabi-cmdline/src/edu/shsu/hanabi_cmdline/database/Database.java
@@ -3,18 +3,22 @@ package edu.shsu.hanabi_cmdline.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Database {
 	
 	private Connection connect;
-	private String name;
+	private String dbname;
 	
 	public Database(String name){
-		setName(name);
+		setDBName(name);
 	}
 
-	private void setName(String name) {
-		this.name = name;
+	private void setDBName(String name) {
+		this.dbname = name.substring(0, name.length() - 3);
+		System.out.println("Database name = " + this.dbname);
 	}
 	
 	public void connect(){
@@ -30,11 +34,117 @@ public class Database {
 	}
 	
 	public void close(){
-		this.close();
+		try {
+			this.connect.close();
+		} catch (SQLException e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+	}
+	
+	public void createTable(String table, LinkedHashMap<String, String> columns){
+		StringBuffer sb = new StringBuffer();
+		String query;
+		sb.append("CREATE TABLE " + table + "(");
+		for(Map.Entry<String, String> entry : columns.entrySet()){
+			String key = entry.getKey();
+			String value = entry.getValue();
+			sb.append(key + " " + value + ",");			
+		}
+		query = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, ")").toString();
+		//this.execute(query);
+		System.out.println(query);
+	}
+	
+	public void insert(String table, String[] values){
+		StringBuffer sb = new StringBuffer();
+		String query;
+		sb.append("INSERT INTO " + table + " VALUES(");
+		for(int i = 0; i < values.length; i++){
+			String v = values[i];
+			if(!this.isInteger(values[i]))
+				sb.append("'" + v + "',");
+			else
+				sb.append(v + ",");
+				
+		}
+		query = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, ")").toString();
+		//this.execute(query);
+		System.out.println(query);
+	}
+	
+	public void update(String table, LinkedHashMap<String, String> setValue, LinkedHashMap<String, String> where){
+		StringBuffer sb = new StringBuffer();
+		String query;
+		sb.append("UPDATE " + table + " SET ");
+		for(Map.Entry<String, String> entry : setValue.entrySet()){
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if(!this.isInteger(value))
+				sb.append(key + "='" + value + "',");
+			else
+				sb.append(key + "=" + value + ",");
+		}
+		sb = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, " ");
+		sb.append("WHERE ");
+		for(Map.Entry<String, String> entry : where.entrySet()){
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if(!this.isInteger(value))
+				sb.append(key + "='" + value + "',");
+			else
+				sb.append(key + "=" + value + ",");
+		}
+		query = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, "").toString();
+		//this.execute(query);
+		System.out.println(query);
+	}
+	
+	public void select(String table, String[] fields, LinkedHashMap<String, String> where){
+		StringBuffer sb = new StringBuffer();
+		String query;
+		sb.append("SELECT ");
+		for(int i = 0; i < fields.length; i++){
+			String v = fields[i];
+			sb.append(v + ",");
+		}
+		sb = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, " ");
+		sb.append("FROM " + table + " WHERE ");
+		for(Map.Entry<String, String> entry : where.entrySet()){
+			String key = entry.getKey();
+			String value = entry.getValue();
+			if(!this.isInteger(value))
+				sb.append(key + "='" + value + "',");
+			else
+				sb.append(key + "=" + value + ",");
+		}
+		query = sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, "").toString();
+		System.out.println(query);
+	}
+	
+	private void execute(String query){
+		Statement statement;
+		try {
+			statement = this.connect.createStatement();
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+	}
+	
+	private boolean isInteger(String s){
+		try{
+			Integer.parseInt(s);
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		return true;
 	}
 
 	public String getName() {
-		return this.name;
+		return this.dbname;
 	}
 	
 
--- hanabi-cmdline/src/edu/shsu/hanabi_cmdline/main.java
+++ hanabi-cmdline/src/edu/shsu/hanabi_cmdline/main.java
@@ -1,5 +1,7 @@
 package edu.shsu.hanabi_cmdline;
 
+import java.util.LinkedHashMap;
+
 import edu.shsu.hanabi_cmdline.database.Database;
 
 public class main {
@@ -7,7 +9,21 @@ public class main {
 	public static void main(String[] args) {
 		
 		Database db = new Database("hanabi.db");
+		LinkedHashMap<String, String> columns = new LinkedHashMap<String, String>();
+		String[] values = {"one", "two", "three"};
+		
+		columns.put("name", "one");
+		columns.put("some", "2");
+		
 		db.connect();
+		db.createTable("One", columns);
+		db.insert("whatever", values);
+		db.update("update", columns, columns);
+		db.select("something", values, columns);
+		db.close();
 	}
-	// what again
+	
+	public void createTempTable(Database db, String admin, int gameID){
+		
+	}
 }
