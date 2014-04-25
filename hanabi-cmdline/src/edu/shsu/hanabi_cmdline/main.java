1d84e04724766482ed5f8fc64b5001bf9fc2802e
alskib <flinkblot@gmail.com>
2014-04-25 11:12:35 -0500

    Added back main from borked pull request

---
+++ hanabi-cmdline/src/edu/shsu/hanabi_cmdline/main.java
@@ -0,0 +1,39 @@
+package edu.shsu.hanabi_cmdline;
+
+import java.util.LinkedHashMap;
+
+import edu.shsu.hanabi_cmdline.database.Database;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		Database db = new Database("hanabi.db");
+		LinkedHashMap<String, String> columns = new LinkedHashMap<String, String>();
+		String[] values = {"one", "two", "3"};
+		
+		columns.put("name", "one");
+		columns.put("some", "2");
+		
+		db.connect();
+		//db.createTable("One", columns);
+		//db.insert("whatever", columns);
+		//db.update("update", columns, columns);
+		//db.select("something", values, columns);
+		db.dropTable("One");
+		db.close();
+	}
+		
+	public void createTempTable(Database db, String admin, int gameID){
+		String name = admin + Integer.toString(gameID);
+		LinkedHashMap<String, String> columns = new LinkedHashMap<String, String>();
+		
+		columns.put("turn", "INTEGER");
+		columns.put("type", "VARCHAR(10)");
+		columns.put("color", "VARCHAR(10)");
+		columns.put("number", "INTEGER");
+		columns.put("curPlayerID", "INTEGER");
+		columns.put("playerGetInfo", "INTEGER");
+		db.createTable(name, columns);
+	}
+}
