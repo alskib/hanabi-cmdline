bc597d274e62597fd484130a830e33e084f222d2
alskib <flinkblot@gmail.com>
2014-04-25 10:50:50 -0500

    Cleaned main

--- hanabi-cmdline/src/edu/shsu/hanabi_cmdline/main.java
+++ hanabi-cmdline/src/edu/shsu/hanabi_cmdline/main.java
@@ -3,8 +3,6 @@ package edu.shsu.hanabi_cmdline;
 public class main {
 	
 	public static void main(String[] args) {
-		DeckDraw deck = new DeckDraw(50);
-		deck.iterateDeck();
-		System.out.println(deck.getCardCount());
+		
 	}
 }
