4e01c0499f9a43b84b379b711adec513be7d1cf9
Johnny Nguyen <nsaneazylm@gmail.com>
2014-04-26 01:53:06 -0500

    Updated comments in TableGame

--- hanabi-cmdline/src/edu/shsu/hanabi_cmdline/database/TableGame.java
+++ hanabi-cmdline/src/edu/shsu/hanabi_cmdline/database/TableGame.java
@@ -9,11 +9,16 @@ public class TableGame {
 
 	private int numPlayers, maxPlayers, finalScore, id;
 	private boolean pvtGame;
-	private String dt, table, password, name;
+	private String dt, table, password;
 	private Database database;
 	private ResultSet resultSet;
 	private Player admin;
 	
+	/**
+	 * Constructor connects the database
+	 * @param dbName Name of database
+	 * @param gameInfo LinkedHashMap containing the game info in &ltfield, value&gt pairs
+	 */
 	public TableGame(Database dbName, LinkedHashMap<String, String> gameInfo){
 		setDatabase(dbName);
 		setAdmin(admin);
@@ -22,18 +27,33 @@ public class TableGame {
 		setGameInfo(admin.getID());
 	}
 	
+	/**
+	 * This function sets the database
+	 * @param dbName
+	 */
 	private void setDatabase(Database dbName){
 		this.database = dbName;
 	}
 	
+	/**
+	 * This function sets the admin
+	 * @param admin
+	 */
 	private void setAdmin(Player admin){
 		this.admin = admin;
 	}
 	
+	/**
+	 * This function sets the Game table
+	 */
 	private void setTable(){
 		this.table = "Game";
 	}
 	
+	/**
+	 * This function sets the rest of the Game info
+	 * @param adminID admin ID from the Game object
+	 */
 	private void setGameInfo(int adminID){
 		String[] fields = {"*"};
 		LinkedHashMap<String, String> where = new LinkedHashMap<String, String>();
@@ -48,6 +68,7 @@ public class TableGame {
 				this.id = this.resultSet.getInt("id");
 				this.pvtGame = this.resultSet.getBoolean("pvtGame");
 				this.password = this.resultSet.getString("password");
+				this.dt = this.resultSet.getString("dtCreated");
 			}
 		} catch (SQLException e) {
 			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
@@ -55,6 +76,10 @@ public class TableGame {
 		}
 	}
 	
+	/**
+	 * This function is used to insert game info into the database
+	 * @param values LinkedHashMap containing game info as &ltfield, value&gt pairs
+	 */
 	public void insertGameInfo(LinkedHashMap<String, String> values){
 		Random idnumber = new Random();
 		String[] fields= {"*"};
@@ -99,5 +124,15 @@ public class TableGame {
 	
 	public String getPassword(){
 		return this.password;
+	}
+	
+	public String getDateTime(){
+		return this.dt;
+	}
+	
+	public boolean isPrivateGame(){
+		if(!this.pvtGame)
+			return false;
+		return true;
 	}
 }
